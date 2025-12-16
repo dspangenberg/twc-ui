026fd61d3e60a13935e46706912b6782229850e5
Danny Spangenberg <danny.spangenberg@twiceware.de>
2025-12-16 02:04:49 +0100

    Some fixes for DropdownButton Demos

--- resources/js/pages/demos/avatar/default.tsx
+++ resources/js/pages/demos/avatar/default.tsx
@@ -4,8 +4,8 @@ import { Avatar } from '@/components/twc-ui/avatar'
 export default function Dashboard() {
   return (
     <DemoContainer>
-      <Avatar src="/ds.png" initials="DS" size="lg" />
-      <Avatar fullname="Danny Spangenberg" size="lg" />
+      <Avatar src="https://github.com/shadcn.png" initials="DS" size="lg" />
+      <Avatar fullname="Homer Simpson" size="lg" />
       <Avatar fullname="Tim Cook" size="lg" />
       <Avatar initials="BS" size="lg" />
     </DemoContainer>
